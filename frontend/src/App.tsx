9c3819ba3152dad746c27c3b6b230b9057d4f3d7
Charlie Chae <chaejune@gmail.com>
2024-02-14 23:58:51 -0500

    register form added.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
@@ -26,7 +27,15 @@ const App = () => {
             </Layout>
           }
         />
-        <Route path="/sign-up" element={<Navigate to="/" />} />
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
